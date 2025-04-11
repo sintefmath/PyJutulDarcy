b7cc3c66756affc5ba19fa885822d0b00dbc836b
Olav Møyner <olav.moyner@gmail.com>
2025-04-11 14:30:40 +0200

    !

--- jutuldarcy/__init__.py
+++ jutuldarcy/__init__.py
@@ -2,7 +2,14 @@ from juliacall import Main as jl, convert as jlconvert
 import numpy as np
 
 # Load the main package + linear solver
-jl.seval("using JutulDarcy, Jutul, HYPRE")
+jl.seval("using JutulDarcy, Jutul, HYPRE, GeoEnergyIO")
 def main():
     """Entry point for the application script"""
     print("Call your main application code here!")
+
+def test_file_path(*args):
+    return jl.GeoEnergyIO.test_input_file_path(*args)
+
+def simulate_data_file(fname, **kwargs):
+    return jl.JutulDarcy.simulate_data_file(fname, **kwargs)
+
