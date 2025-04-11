83f300f18ae8b979b9da84cd64d7d8962a8fce1e
Olav Møyner <olav.moyner@gmail.com>
2025-04-11 13:02:59 +0200

    Update __init__.py

--- jutuldarcy/__init__.py
+++ jutuldarcy/__init__.py
@@ -1,3 +1,6 @@
+from juliacall import Main as jl, convert as jlconvert
+import numpy as np
+
 def main():
     """Entry point for the application script"""
-    print("Call your main application code here")
+    print("Call your main application code here!")
