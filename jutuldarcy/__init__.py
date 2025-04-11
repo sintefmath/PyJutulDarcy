f968cd6088f555aad06ccf83b03405aecf7b6895
Olav Møyner <olav.moyner@gmail.com>
2025-04-11 14:58:44 +0200

    Update __init__.py

--- jutuldarcy/__init__.py
+++ jutuldarcy/__init__.py
@@ -3,9 +3,6 @@ import numpy as np
 
 # Load the main package + linear solver
 jl.seval("using JutulDarcy, Jutul, HYPRE, GeoEnergyIO")
-def main():
-    """Entry point for the application script"""
-    print("Call your main application code here!")
 
 def test_file_path(*args):
     return jl.GeoEnergyIO.test_input_file_path(*args)
@@ -19,11 +16,28 @@ def simulate_data_file(fname, convert = True, **kwargs):
     return out
 
 def convert_to_pydict(result):
-    # Convert wells
-    jl.JutulDarcy.reservoir_measurables(model, wellresult, states)
-    c = result.extra[jd.jl.Symbol("case")]
-    smry = jd.jl.JutulDarcy.summary_result(c, result)
-    # Convert field measurables
-    # Convert states
-    error()
-    return 0
+    c = result.extra[jl.Symbol("case")]
+    smry = jl.JutulDarcy.summary_result(c, result)
+
+    wdict = dict()
+    for k in jl.keys(smry["VALUES"]["WELLS"]):
+        wdict[k] = _numdict_to_py(smry["VALUES"]["WELLS"][k])
+    fdict = _numdict_to_py(smry["VALUES"]["FIELD"])
+
+    out = dict()
+    out["WELLS"] = wdict
+    out["FIELD"] = fdict
+    day = 24*3600.0
+    out["DAYS"] = np.array(np.divide(smry["TIME"].seconds, day))
+    pystates = []
+    for state in result.states:
+        pystates.append(_numdict_to_py(state))
+    out["STATES"] = pystates
+
+    return out
+
+def _numdict_to_py(d_jl):
+    d_py = dict()
+    for k in jl.keys(d_jl):
+        d_py[jl.String(k)] = np.array(d_jl[k])
+    return d_py
