29d44ab8b1c4a9a35aefc4f1c63698fa80b25523
Olav Møyner <olav.moyner@gmail.com>
2025-04-11 16:14:30 +0200

    Add fixes borrowed from PySR for juliacall issues

--- jutuldarcy/__init__.py
+++ jutuldarcy/__init__.py
@@ -1,3 +1,40 @@
+
+# The following code that handles juliacall issues is copied from PySR
+# Copyright 2020 Miles Cranmer, Apache 2.0 License.
+# Source: https://github.com/MilesCranmer/PySR/blob/cd055a67728eeb675c76dedfe5d5e669eea3a6d1/pysr/julia_import.py
+import os
+import sys
+import warnings
+if "juliacall" in sys.modules:
+    warnings.warn(
+        "juliacall module already imported. "
+        "Make sure that you have set the environment variable `PYTHON_JULIACALL_HANDLE_SIGNALS=yes` to avoid segfaults. "
+        "Also note that PySR will not be able to configure `PYTHON_JULIACALL_THREADS` or `PYTHON_JULIACALL_OPTLEVEL` for you."
+    )
+else:
+    # Required to avoid segfaults (https://juliapy.github.io/PythonCall.jl/dev/faq/)
+    if os.environ.get("PYTHON_JULIACALL_HANDLE_SIGNALS", "yes") != "yes":
+        warnings.warn(
+            "PYTHON_JULIACALL_HANDLE_SIGNALS environment variable is set to something other than 'yes' or ''. "
+            + "You will experience segfaults if running with multithreading."
+        )
+
+    if os.environ.get("PYTHON_JULIACALL_THREADS", "auto") != "auto":
+        warnings.warn(
+            "PYTHON_JULIACALL_THREADS environment variable is set to something other than 'auto', "
+            "so PySR was not able to set it. You may wish to set it to `'auto'` for full use "
+            "of your CPU."
+        )
+
+    # TODO: Remove these when juliapkg lets you specify this
+    for k, default in (
+        ("PYTHON_JULIACALL_HANDLE_SIGNALS", "yes"),
+        ("PYTHON_JULIACALL_THREADS", "auto"),
+        ("PYTHON_JULIACALL_OPTLEVEL", "3"),
+    ):
+        os.environ[k] = os.environ.get(k, default)
+
+# Actual start of module - now that juliacall can be imported
 from juliacall import Main as jl, convert as jlconvert
 import numpy as np
 
