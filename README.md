55d38631fbfe93ac5cf36b95c90324c867775b8c
Olav Møyner <olav.moyner@gmail.com>
2025-04-11 15:42:20 +0200

    First working version of module (#1)

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# pyjutuldarcy
+# PyJutulDarcy
+
+Python wrapper for [JutulDarcy.jl, a fully differentiable reservoir simulator written in Julia](https://github.com/sintefmath/JutulDarcy.jl). This package facilitates automatic installation of JutulDarcy from Python, as well as a minimal interface that allows fast simulation of .DATA files in pure Python. For more details about JutulDarcy.jl, please see the [Julia Documentation](https://sintefmath.github.io/JutulDarcy.jl/dev/).
+
+The package also provides access to all the functions of the Julia version under `jutuldarcy.jl.JutulDarcy`, `jutuldarcy.jl.GeoEnergyIO` and `jutuldarcy.jl.Jutul`. These functions are directly wrapped using [JuliaCall](https://github.com/JuliaPy/PythonCall.jl). For more details, see the [JuliaCall Documentation on converting of types](https://juliapy.github.io/PythonCall.jl/stable/conversion-to-julia/).
+
+## Installation
+
+The package can be installed with pip:
+
+```julia
+pip install jutuldarcy
+```
+
+On first time usage of the package [JuliaCall](https://github.com/JuliaPy/PythonCall.jl) will automatically install Julia and manage all dependency packages.
+
+## A minimal example
+
+```python
+import jutuldarcy as jd
+# Load SPE9 dataset to disk
+pth = jd.test_file_path("SPE9", "SPE9.DATA")
+# Simulate the model
+res = jd.simulate_data_file(pth)
+# Get field quantities and plot
+import matplotlib.pyplot as plt
+fopr = res["FIELD"]["FOPR"]
+days = res["DAYS"]
+plt.plot(days, fopr)
+plt.ylabel("Field oil production")
+plt.xlabel("Days")
+plt.show()
+```
+
+<img width="299" alt="pyplot_fopr" src="https://github.com/user-attachments/assets/9b69bdee-91d9-4b37-ba20-05725f224cd9" />
+
+Here, res is a standard dict containing the following fields:
+
+- "FIELD": Field quantities (average pressure, total water injection, etc) as numpy arrays.
+- "WELLS": Well quantities (bottom hole pressures, injection rates, production rates, etc)
+- "STATES": Reservoir states for all active cells, given as a list with a dict for each timestep. For example, `res["STATES"][10]["Rs"]` will give you an array of the solution gas-oil-ratio at step 10.
+- "DAYS": Array of the number of days elapsed for each step.
+
+Optionally, `convert = False` can be passed to get access to the "full" output as seen in Julia, where it is possible to get access to grid geometry, model parameters, and so on.
+
+## Paper and citing
+
+The main paper describing `JutulDarcy.jl` is *JutulDarcy.jl - a Fully Differentiable High-Performance Reservoir Simulator Based on Automatic Differentiation*:
+
+```bibtex
+@article{jutuldarcy_ecmor_2024,
+   author = "M{\o}yner, O.",
+   title = "JutulDarcy.jl - a Fully Differentiable High-Performance Reservoir Simulator Based on Automatic Differentiation", 
+   year = "2024",
+   volume = "2024",
+   number = "1",
+   pages = "1-9",
+   doi = "https://doi.org/10.3997/2214-4609.202437111",
+   publisher = "European Association of Geoscientists \& Engineers",
+   issn = "2214-4609",
+}
+```
+
+## Contributing
+
+Contributions that expose additional functionality is welcome.
---
+++ jutuldarcy/__init__.py
@@ -0,0 +1,90 @@
+from juliacall import Main as jl, convert as jlconvert
+import numpy as np
+
+# Load the main package + linear solver
+jl.seval("using JutulDarcy, Jutul, HYPRE, GeoEnergyIO")
+
+def test_file_path(*args):
+    """
+    Get the path to a test file in the GeoEnergyIO package. This may trigger a
+    download if it is the first time the given example file is requested. If a
+    path is returned, the file is available on-disk at that location.
+
+    Examples:
+    test_file_path("SPE1", "SPE1.DATA")
+    test_file_path("SPE9", "SPE9.DATA")
+    test_file_path("OLYMPUS_1", "OLYMPUS_1.DATA")
+    test_file_path("NORNE_NOHYST", "NORNE_NOHYST.DATA")
+    test_file_path("EGG", "EGG.DATA")
+
+    """
+    return jl.GeoEnergyIO.test_input_file_path(*args)
+
+def simulate_data_file(data_file_name, convert = True, units = jl.missing, **kwargs):
+    """
+    Simulate a reservoir .DATA file using JutulDarcy and return the results as a
+    Python dictionary. The resulting dictionary has the following fields:
+
+    - WELLS: A dictionary of well results, where each key is a well name and the
+      value is a dictionary of results for that well.
+    - FIELD: A dictionary of field results.
+    - DAYS: A numpy array of time in days.
+    - STATES: A list of dictionaries, each representing the state of the reservoir
+      at a given time step. The field vary based on the type of simulation.
+    
+    If you are looking for files to test, see test_file_path() for a few
+    examples of files to test.
+
+    Examples:
+    pth = test_file_path("SPE1", "SPE1.DATA")
+    # Simulate the data file and convert to Python dictionary
+    res = jd.simulate_data_file(pth, units = "field", restart = True, output_path = "/tmp")
+    # Write output to disk and apply restart (restarting if simulation was aborted, otherwise retrieving results from disk)
+    import tempfile
+    res = jd.simulate_data_file(pth, restart = True, output_path = tempfile.gettempdir())
+    # Simulate and change the units to field units even if input file has other units
+    res = jd.simulate_data_file(pth, units = "field")
+
+    """
+    units = _convert_units(units)
+    res = jl.JutulDarcy.simulate_data_file(data_file_name, **kwargs)
+    if convert:
+        out = convert_to_pydict(res, units=units)
+    else:
+        out = res
+    return out
+
+def convert_to_pydict(result, units = jl.missing):
+    c = result.extra[jl.Symbol("case")]
+    units = _convert_units(units)
+    smry = jl.JutulDarcy.summary_result(c, result, units)
+
+    wdict = dict()
+    for k in jl.keys(smry["VALUES"]["WELLS"]):
+        wdict[k] = _numdict_to_py(smry["VALUES"]["WELLS"][k])
+    fdict = _numdict_to_py(smry["VALUES"]["FIELD"])
+
+    out = dict()
+    out["WELLS"] = wdict
+    out["FIELD"] = fdict
+    day = 24*3600.0
+    out["DAYS"] = np.array(np.divide(smry["TIME"].seconds, day))
+    pystates = []
+    for state in result.states:
+        pystates.append(_numdict_to_py(state))
+    out["STATES"] = pystates
+
+    return out
+
+def _numdict_to_py(d_jl):
+    d_py = dict()
+    for k in jl.keys(d_jl):
+        d_py[jl.String(k)] = np.array(d_jl[k])
+    return d_py
+
+def _convert_units(units):
+    if isinstance(units, str):
+        if units not in ["field", "si", "metric", "lab"]:
+            raise RuntimeError("units must be one of field, si, metric, lab")
+        units = jl.Symbol(units)
+    return units
