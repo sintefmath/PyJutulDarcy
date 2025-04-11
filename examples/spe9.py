8fe37608bb98e0453fe249a967786cd7b8689543
Olav Møyner <olav.moyner@gmail.com>
2025-04-11 15:38:06 +0200

    Example

---
+++ examples/spe9.py
@@ -0,0 +1,12 @@
+import jutuldarcy as jd
+import matplotlib.pyplot as plt
+
+pth = jd.test_file_path("SPE9", "SPE9.DATA")
+res = jd.simulate_data_file(pth)
+
+fopr = res["FIELD"]["FOPR"]
+days = res["DAYS"]
+plt.plot(days, fopr)
+plt.ylabel("Field oil production")
+plt.xlabel("Days")
+plt.show()
