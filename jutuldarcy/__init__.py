56c0686bd8f0ff758284aa83140eb7a3ffa895e3
Olav Møyner <olav.moyner@gmail.com>
2025-04-11 14:42:10 +0200

    Update __init__.py

--- jutuldarcy/__init__.py
+++ jutuldarcy/__init__.py
@@ -10,6 +10,20 @@ def main():
 def test_file_path(*args):
     return jl.GeoEnergyIO.test_input_file_path(*args)
 
-def simulate_data_file(fname, **kwargs):
-    return jl.JutulDarcy.simulate_data_file(fname, **kwargs)
+def simulate_data_file(fname, convert = True, **kwargs):
+    res = jl.JutulDarcy.simulate_data_file(fname, **kwargs)
+    if convert:
+        out = convert_to_pydict(res)
+    else:
+        out = res
+    return out
 
+def convert_to_pydict(result):
+    # Convert wells
+    jl.JutulDarcy.reservoir_measurables(model, wellresult, states)
+    c = result.extra[jd.jl.Symbol("case")]
+    smry = jd.jl.JutulDarcy.summary_result(c, result)
+    # Convert field measurables
+    # Convert states
+    error()
+    return 0
