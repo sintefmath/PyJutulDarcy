ec287ec10b9985249e6aa8c3522afbfe9c86f06e
Olav Møyner <olav.moyner@gmail.com>
2025-04-14 19:50:01 +0200

    Replace name in copied code

--- jutuldarcy/__init__.py
+++ jutuldarcy/__init__.py
@@ -9,7 +9,7 @@ if "juliacall" in sys.modules:
     warnings.warn(
         "juliacall module already imported. "
         "Make sure that you have set the environment variable `PYTHON_JULIACALL_HANDLE_SIGNALS=yes` to avoid segfaults. "
-        "Also note that PySR will not be able to configure `PYTHON_JULIACALL_THREADS` or `PYTHON_JULIACALL_OPTLEVEL` for you."
+        "Also note that jutuldarcy will not be able to configure `PYTHON_JULIACALL_THREADS` or `PYTHON_JULIACALL_OPTLEVEL` for you."
     )
 else:
     # Required to avoid segfaults (https://juliapy.github.io/PythonCall.jl/dev/faq/)
@@ -22,7 +22,7 @@ else:
     if os.environ.get("PYTHON_JULIACALL_THREADS", "auto") != "auto":
         warnings.warn(
             "PYTHON_JULIACALL_THREADS environment variable is set to something other than 'auto', "
-            "so PySR was not able to set it. You may wish to set it to `'auto'` for full use "
+            "so jutuldarcy was not able to set it. You may wish to set it to `'auto'` for full use "
             "of your CPU."
         )
 
