569bb611739af304705479440bc8d6fee8480b5e
Olav Møyner <olav.moyner@gmail.com>
2025-04-15 10:18:40 +0200

    Updates

---
+++ examples/standalone.py
@@ -0,0 +1,67 @@
+import importlib
+import jutuldarcy as jd
+import numpy as np
+importlib.reload(jd)
+
+day = jd.si_unit("day")
+Darcy = jd.si_unit("darcy")
+kg = jd.si_unit("kilogram")
+meter = jd.si_unit("meter")
+bar = jd.si_unit("bar")
+
+
+nx = ny = 25
+nz = 10
+cart_dims = (nx, ny, nz)
+physical_dims = (1000.0*meter, 1000.0*meter, 100.0*meter)
+
+g = jd.CartesianMesh(cart_dims, physical_dims)
+domain = jd.reservoir_domain(g, permeability = 0.3*Darcy, porosity = 0.2)
+Injector = jd.setup_vertical_well(domain, 1, 1, name = "Injector")
+Producer = jd.setup_well(domain, (nx, ny, 1), name = "Producer")
+# Show the properties in the domain
+phases = (jd.LiquidPhase(), jd.VaporPhase())
+rhoLS = 1000.0*kg/meter**3
+rhoGS = 100.0*kg/meter**3
+reference_densities = [rhoLS, rhoGS]
+sys = jd.ImmiscibleSystem(phases, reference_densities = reference_densities)
+
+model, parameters = jd.setup_reservoir_model(domain, sys, wells = [Injector, Producer])
+
+
+c = np.array([1e-6/bar, 1e-4/bar])
+density = jd.jl.ConstantCompressibilityDensities(
+    p_ref = 100*bar,
+    density_ref = reference_densities,
+    compressibility = c
+)
+kr = jd.jl.BrooksCoreyRelativePermeabilities(sys, np.array([2.0, 3.0]))
+jd.replace_variables(model, PhaseMassDensities = density, RelativePermeabilities = kr)
+
+
+state0 = jd.setup_reservoir_state(model,
+    Pressure = 120*bar,
+    Saturations = np.array([1.0, 0.0])
+)
+
+nstep = 25
+dt = np.repeat(365.0*day, nstep)
+
+
+pv = jd.pore_volume(model, parameters)
+inj_rate = 1.5*np.sum(pv)/sum(dt)
+
+I_ctrl = jd.setup_injector_control(inj_rate, "rate", np.array([0.0, 1.0]), density = rhoGS)
+P_ctrl = jd.setup_producer_control(100*bar, "bhp")
+
+controls = dict(Injector = I_ctrl, Producer = P_ctrl)
+forces = jd.setup_reservoir_forces(model, control = controls)
+
+result = jd.simulate_reservoir(state0, model, dt, parameters = parameters, forces = forces)
+
+# To run the following:
+# 1. Run in an environment that supports plotting (OpenGL capable)
+# 2. Run jd.install_plotting()
+# 
+plt = jd.plot_reservoir(model, result.states)
+jd.make_interactive()
--- jutuldarcy/plotting.py
+++ jutuldarcy/plotting.py
@@ -16,6 +16,11 @@ def install_plotting():
     activate_plotting()
     return True
 
+def uninstall_plotting():
+    juliapkg.rm("GLMakie", "e9467ef8-e4e7-5192-8a1a-b1aee30e663a")
+    juliapkg.resolve()
+    return True
+
 def plot_reservoir(*arg, **kwarg):
     if activate_plotting():
         return jl.plot_reservoir(*arg, **kwarg)
