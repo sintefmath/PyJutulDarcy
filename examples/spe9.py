309b660978a701e9d615a4b6bc9378928908084c
Olav Møyner <olav.moyner@gmail.com>
2025-04-15 10:18:50 +0200

    Update spe9.py

--- examples/spe9.py
+++ examples/spe9.py
@@ -2,7 +2,7 @@ import jutuldarcy as jd
 import matplotlib.pyplot as plt
 
 pth = jd.test_file_path("SPE9", "SPE9.DATA")
-res = jd.simulate_data_file(pth)
+res = jd.simulate_data_file(pth, convert = True)
 
 fopr = res["FIELD"]["FOPR"]
 days = res["DAYS"]
