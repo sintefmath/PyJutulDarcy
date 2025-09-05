959a4deaefc62b12f0c976333f807ded130e4607
havahol <havard.heitlo.holm@sintef.no>
2025-09-05 11:32:01 +0200

    notebook for hydrostatic equilibrium with required wrapper additions

--- jutuldarcy/reservoir.py
+++ jutuldarcy/reservoir.py
@@ -2,3 +2,6 @@ from .julia_import import jl
 
 def reservoir_domain(*arg, **kwargs):
     return jl.reservoir_domain(*arg, **kwargs)
+
+def number_of_cells(*arg, **kwargs):
+    return jl.number_of_cells(*arg, **kwargs)
